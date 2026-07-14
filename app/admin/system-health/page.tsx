90f3337713d3f106fd211fbefedcbf93870da0cc
Shiro <info@shirotechnologies.com>
2026-07-14 18:35:03 +0530

    fix: lerpColor return hex not rgb() so alpha appending works

--- app/admin/system-health/page.tsx
+++ app/admin/system-health/page.tsx
@@ -183,7 +183,7 @@ function MasterECG({ overallStatus, brandColors }: { overallStatus: string; bran
       const r    = Math.round(((a >> 16) & 255) * (1 - t) + ((b >> 16) & 255) * t)
       const g    = Math.round(((a >>  8) & 255) * (1 - t) + ((b >>  8) & 255) * t)
       const bl   = Math.round(( a        & 255) * (1 - t) + ( b        & 255) * t)
-      return `rgb(${r},${g},${bl})`
+      return '#' + [r, g, bl].map(v => v.toString(16).padStart(2, '0')).join('')
     }
 
     const draw = () => {
