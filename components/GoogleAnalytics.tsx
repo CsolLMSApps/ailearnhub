31da4fea8a452eae104576e8fc658716ef6de06c
Shiro <info@shirotechnologies.com>
2026-08-03 21:38:36 +0530

    fix: set GA4 consent default to denied before script loads (GDPR)

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -5,6 +5,23 @@ import Script from 'next/script'
 export default function GoogleAnalytics() {
   return (
     <>
+      {/* Consent defaults must be set BEFORE the GA4 script loads */}
+      <Script id="google-analytics-consent-default" strategy="beforeInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+
+          // Check if user has already consented
+          var consent = typeof localStorage !== 'undefined' ? localStorage.getItem('cookieConsent') : null;
+
+          gtag('consent', 'default', {
+            analytics_storage: consent === 'accepted' ? 'granted' : 'denied',
+            ad_storage: 'denied',
+            wait_for_update: 500
+          });
+        `}
+      </Script>
+
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-BRP7N4DX9C"
         strategy="afterInteractive"
